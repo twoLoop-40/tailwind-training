582fe5b1c490d619e4adabce937ae0525a077830
Joon Ho Lee <jh.lee@spira-t.com>
2022-12-21 21:15:23 +0900

    base change

--- components/BaseLayout.tsx
+++ components/BaseLayout.tsx
@@ -2,6 +2,6 @@ import styled from "styled-components";
 import tw from "twin.macro";
 
 export const Wrapper = styled.div`
-  ${tw`sm:w-80 max-w-lg p-4 flex-col h-full`}
+  ${tw`w-80 max-w-lg p-4 flex-col h-full`}
   background-color: ${(props) => (props.color ? props.color : "white")};
 `;
